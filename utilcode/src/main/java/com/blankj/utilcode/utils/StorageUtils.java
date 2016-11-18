d66991957399d0a14210d9f61ec769a0b5b86cdc
jiajia77 <2604425169@qq.com>
2016-11-18 08:38:36 +0800

    Create StorageUtils.java

---
+++ utilcode/src/main/java/com/blankj/utilcode/utils/StorageUtils.java
@@ -0,0 +1,45 @@
+package com.aritime.aridj.utils;
+
+import android.os.Environment;
+import android.os.StatFs;
+
+import java.io.File;
+
+/**
+ * Created by jiajia on 2016/11/17.
+ */
+
+public class StorageUtils {
+
+    File dataFileDir = Environment.getDataDirectory();// 手机内存路径
+    File dataSDFileDir = Environment.getExternalStorageDirectory();// SD卡存储路径
+    
+    /**
+     * 根据路径获取总存储空间
+     * @param path
+     * @return string 空间大小
+     */
+    public static String getMemoryInfo(File path) {
+
+        StatFs stat = new StatFs(path.getPath());// 获得一个磁盘状态对象
+        long blockSize = stat.getBlockSizeLong ();   // 获得一个扇区的大小
+        long totalBlocks = stat.getBlockCountLong();    // 获得扇区的总数
+        // 总空间
+        return  ConvertUtils.byte2FitSize(totalBlocks * blockSize);
+    }
+    
+    /**
+     * 根据路径获取剩余存储空间
+     * @param path
+     * @return string 空间大小
+     */
+    public static String getMemoryFree(File path) {
+
+        StatFs stat = new StatFs(path.getPath()); // 获得一个磁盘状态对象
+        long blockSize = stat.getBlockSizeLong ();   // 获得一个扇区的大小
+        long availableBlocks = stat.getAvailableBlocksLong();   // 获得可用的扇区数量
+        // 剩余空间
+        return ConvertUtils.byte2FitSize(availableBlocks * blockSize);
+
+    }
+}
